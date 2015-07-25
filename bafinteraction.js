861702d42269f6acf09f910a83abba1b27c458e7
perambler <perambler@users.noreply.github.com>
2015-07-25 11:49:21 -0700

    Update bafinteraction.js

--- bafinteraction.js
+++ bafinteraction.js
@@ -4,17 +4,8 @@ window.location.replace(...)
 
 
 // Drag and drop code
+$(init);
 
-function allowDrop(ev) {
-    ev.preventDefault();
-}
-
-function drag(ev) {
-    ev.dataTransfer.setData("text", ev.target.id);
-}
-
-function drop(ev) {
-    ev.preventDefault();
-    var data = ev.dataTransfer.getData("text");
-    ev.target.appendChild(document.getElementById(data));
+function init() {
+    $('.dragme').draggable();
 }
